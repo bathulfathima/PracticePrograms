742119ac9387185c7617aa70c43e5342aad8ad87
Bathul Fathima.S <sbathulshaik73@gmail.com>
2026-09-08 10:46:32 +0530

    Create Minus Two.cpp

---
+++ Minus Two.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<queue>
+#include<vector>
+using namespace std;
+#define ll long long int
+ 
+void solve(){
+    int N;
+    cin >> N;
+    int odd = 0, eoq = 0, eeq = 0;
+    for(int ctr = 1; ctr <= N; ctr++){
+        int curr;
+        cin >> curr;
+        if(curr & 1){
+            odd++;
+        }
+        else{
+            int q = curr / 2;
+            if(q & 1){
+                eoq++;
+            }
+            else{
+                eeq++;  
+            }
+        }
+    }
+    cout << max(odd, max(eoq, eeq)) << endl;
+}
+ 
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int T = 1;
+    cin >> T;
+ 
+    while(T--)
+        solve();
+}
